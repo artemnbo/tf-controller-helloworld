8b8abd2ef0a0da6173244e0cc86792b50de95581
ArtemBo <44843067+artemnbo@users.noreply.github.com>
2023-12-05 13:39:13 -0600

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,13 @@
 terraform {
-  required_version = ">= 0.12.26"
+  required_version = ">= 1.1.0"
+  cloud {
+    hostname = "app.terraform.io"
+    organization = "artem-bondarenko-test-project"
+
+    workspaces {
+      name = "tf-test"
+    }
+  }
 }
 
 variable "subject" {
